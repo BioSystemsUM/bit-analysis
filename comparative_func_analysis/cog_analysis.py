46df4f0cb995cf7c267eca8005a0c4cccbd8d4e0
iquasere <maildosequeira@gmail.com>
2021-05-17 16:45:48 +0100

    [REFACTOR] Group in function [WILDCARD] The entire Internet.

--- comparative_func_analysis/cog_analysis.py
+++ comparative_func_analysis/cog_analysis.py
@@ -14,29 +14,25 @@ dbs = {report.iloc[i]['name'] : report.iloc[i]['domain'] for i in range(len(repo
 cog_protein_descriptions = dict()
 names = list()
 
-pbar = ProgressBar()
-for file in pbar(glob.glob('bit_analysis/recognizer_outs/*/COG_report.tsv')):
-    name = file.split('/')[2]; names.append(name)
-    data = pd.read_csv(f'bit_analysis/recognizer_outs/{name}/COG_report.tsv', sep='\t')  #(f'bit_analysis/recognizer_outs/{name}/{dbs[name].upper()}_report.tsv')
-    cog_protein_descriptions[name] = data[data['COG general functional category'] == 'METABOLISM']['DB ID'].tolist()
-    #functions = set(data[f'{dbs[name].upper()} protein description'])
+def boolean_matrix(dictionary, names):
+    cpd = list()
+    for v in dictionary.values():
+        cpd += v
 
-cpd = list()
-for v in cog_protein_descriptions.values():
-    cpd += v
+    names = set(names)
+    result = pd.DataFrame(index = range(len(set(cpd))))
+    for name in names:
+        result = pd.concat([result, pd.Series([0] * 1598, name=name)],axis=1)
+    result.index = set(cpd)
+    result = result.transpose()
 
-names = set(names)
-result = pd.DataFrame(index = range(len(set(cpd))))
-for name in names:
-    result = pd.concat([result, pd.Series([0] * 1598, name=name)],axis=1)
-result.index = set(cpd)
-result = result.transpose()
+    for k, v in dictionary.items():
+        print(k)
+        pbar = ProgressBar()
+        for prot in pbar(v):
+            result.loc[k][prot] = 1
 
-for k, v in cog_protein_descriptions.items():
-    print(k)
-    pbar = ProgressBar()
-    for prot in pbar(v):
-        result.loc[k][prot] = 1
+    return result
 
 def distance_between_rows(row1, row2):
     return (result.loc[row1] - result.loc[row2]).abs().sum()
@@ -47,6 +43,16 @@ def distance_matrix(df):
         for taxon2 in df.index:
             result.loc[taxon1, taxon2] = distance_between_rows(taxon1, taxon2)
     return result
+
+pbar = ProgressBar()
+for file in pbar(glob.glob('bit_analysis/recognizer_outs/*/COG_report.tsv')):
+    name = file.split('/')[2]; names.append(name)
+    data = pd.read_csv(f'bit_analysis/recognizer_outs/{name}/COG_report.tsv', sep='\t')
+    cog_protein_descriptions[name] = data[data['COG general functional category'] == 'METABOLISM']['DB ID'].tolist()
+
+boolean_matrix = boolean_matrix(cog_protein_descriptions, names)
+
+distance_matrix = distance_matrix(boolean_matrix)
 
 
 
